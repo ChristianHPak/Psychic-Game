779f2253b4f328299382daa01df6ad24d46b64f5
Christian Pak <iamchristianpak@gmail.com>
2019-05-29 16:20:07 -0600

    started basics of psychic game

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// variables and counters
+var wins = 0;
+var losses = 0;
+var guesseslef = 9;
+
+//array of potentail choices
+
+//variables to hold the reference
+
+//function when key is pressed
